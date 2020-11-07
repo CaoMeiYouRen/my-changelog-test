c4bd9b7ae64047ee249dd6c2e65219b7f69387f4
CaoMeiYouRen <996881204@qq.com>
2020-11-07 16:21:43 +0800

    chore: 对 dev 分支做了修改

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,4 @@
 import './fs-extra'
 console.log('hello world')
 console.log('新功能3.0')
-/**
- *
- * @todo 完善函数内容
- * @author CaoMeiYouRen
- * @date 2020-11-07
- */
-function test() {
-
-}
+// 对 dev 分支做了修改
