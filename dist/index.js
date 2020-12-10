f702ea2f3c7808da5068626f68dceb380d90c91d
CaoMeiYouRen <996881204@qq.com>
2020-12-10 22:51:54 +0800

    chore: 移除 dist

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// import './download'
-// import './http'
-require("./fs-extra");
-console.log('hello world');
-console.log('新功能3.0');
